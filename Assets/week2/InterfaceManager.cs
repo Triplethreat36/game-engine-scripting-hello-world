49a977cc86c295ade8c9f54cd7dca0fe2ac4ef78
Triplethreat36 <149103630+Triplethreat36@users.noreply.github.com>
2024-02-01 20:29:40 -0600

    3

--- Assets/week2/InterfaceManager.cs
+++ Assets/week2/InterfaceManager.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class interface : MonoBehaviour
+public class InterfaceManager : MonoBehaviour
 {
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Upfate");
     }
 }
