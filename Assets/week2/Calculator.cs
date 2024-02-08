d357106638e11fa900e1344cd7c7e334dd5457a2
Triplethreat36 <149103630+Triplethreat36@users.noreply.github.com>
2024-02-07 19:48:46 -0600

    code comments and thoughts and what lines do

--- Assets/week2/Calculator.cs
+++ Assets/week2/Calculator.cs
@@ -5,11 +5,12 @@ using Unity.VisualScripting;
 public class Calculator : MonoBehaviour
 {
     //TODO: Create a TextMeshProUGUI variable here and assign it in the inspector
+
     public TextMeshProUGUI myTextMeshPro;
 
     //TODO: Create a temporary float variable here called prevInput so we can store the previous input value
     //      when performaing calculations
- 
+  
     private float prevInput;
     
     //TODO: Create a bool variable called clearPrevInput here so we can flip it to true/false if we should clear the prior input
@@ -61,6 +62,7 @@ public class Calculator : MonoBehaviour
     {
         //TODO: Store the current
         // input value on the text label into the float variable you created.
+        // these lines of code is when a button is click then set public void Calculate to as public void public void SetEquationAsAdd()
         if (float.TryParse(myTextMeshPro.text, out float parsedInput))
         {
             prevInput = parsedInput;
@@ -69,8 +71,8 @@ public class Calculator : MonoBehaviour
         //      Hint. You will need to google float.Parse() and pass in the string value of the label.
         //TODO: Set the bool you made to true so that the next number that gets typed in clears the calculator display.
         prevInput = float.Parse(text.text);
-        clearPrevInput = true;
-        equationType = EquationType.ADD;
+        clearPrevInput = true;  //clear any other options
+        equationType = EquationType.ADD; // set it to add
     }
     public TextMeshProUGUI text;
 
@@ -79,15 +81,15 @@ public class Calculator : MonoBehaviour
     //      Make sure you set equationType to EquationType.SUBTRACT
     public void SetEquationAsSubtract()
     {
-   
+        // these lines of code is when a button is click then set public void Calculate to as public void SetEquationAsSubtract()
         if (float.TryParse(myTextMeshPro.text, out float parsedInput))
         {
             prevInput = parsedInput;
         }
 
-        clearPrevInput = true;
+        clearPrevInput = true;  //clear any other options
 
-        equationType = EquationType.SUBTRACT;
+        equationType = EquationType.SUBTRACT; // set it as subtract
     }
 
 
@@ -95,35 +97,35 @@ public class Calculator : MonoBehaviour
     //      Make sure you set equationType to EquationType.Multiply
     public void SetEquationAsMultiply()
     {
-        
+        // these lines of code is when a button is click then set public void Calculate to as public void SetEquationAsMultipl
         if (float.TryParse(myTextMeshPro.text, out float parsedInput))
         {
             prevInput = parsedInput;
         }
 
   
-        clearPrevInput = true;
+        clearPrevInput = true; //clear any other options
 
-        equationType = EquationType.MULTIPLY;
+        equationType = EquationType.MULTIPLY; // set it as multiply 
     }
 
     //TODO: Create a SetEquationAsDivide function similar to SetEquationAsAdd.
     //      Make sure you set equationType to EquationType.DIVIDE
     public void SetEquationAsDivide()
     {
-        
+        // these lines of code is when a button is click then set public void Calculate to as   public void SetEquationAsDivide()  
         if (float.TryParse(myTextMeshPro.text, out float parsedInput))
         {
             prevInput = parsedInput;
         }
-        clearPrevInput = true;
+        clearPrevInput = true; //clear any other options
 
-        equationType = EquationType.DIVIDE;
+        equationType = EquationType.DIVIDE; // set it as divide
     }
     public void Add()
     {
-        //TODO: Calculate the sum of the float variable that stores the previous input value and the current input value
-        //      Set the text label to display that sum
+        // this codd allows you to add 2 number together
+
         float currentInput = float.Parse(myTextMeshPro.text);
         float result = prevInput + currentInput;
         myTextMeshPro.text = result.ToString();
@@ -131,9 +133,9 @@ public class Calculator : MonoBehaviour
 
     //TODO: Implement Subtract function
 
-    public void Subtract()
+    public void Subtract() // create the subtract function
     {
-     
+     // these lines of code is allows you to subtract and  
         float currentInput = float.Parse(myTextMeshPro.text);
         float result = prevInput - currentInput;
         myTextMeshPro.text = result.ToString();
@@ -141,7 +143,7 @@ public class Calculator : MonoBehaviour
     //TODO: Implement Multiply function
     public void Multiply()
     {
-        
+        //this code allows you to mutiply 2 number
         float currentInput = float.Parse(myTextMeshPro.text);
         float result = prevInput * currentInput;
         myTextMeshPro.text = result.ToString();
@@ -150,7 +152,7 @@ public class Calculator : MonoBehaviour
     //TODO: Implement Divide function
     public void Divide()
     {
-     
+     //this code allows you to divide 2 numbers unless it 0
         float currentInput = float.Parse(myTextMeshPro.text);
 
         if (currentInput != 0f) 
@@ -169,9 +171,10 @@ public class Calculator : MonoBehaviour
     {
         //TODO: Reset the state of your calculator... reset the display value to a 0, reset the bool variable
         //      that represents if the display should be cleared to true, reset the temporary float variable as well to 0
-        myTextMeshPro.text = "0";
+        myTextMeshPro.text = "0"; // when it cleared make it 0
         clearPrevInput = true;
         prevInput = 0f;
+        // this code sets text back to 0 if the button is clicked
 
         //TODO: Leave this alone
         equationType = EquationType.None;        
@@ -184,7 +187,8 @@ public class Calculator : MonoBehaviour
         else if (equationType == EquationType.SUBTRACT) Subtract();
         else if (equationType == EquationType.MULTIPLY) Multiply();
         else if (equationType == EquationType.DIVIDE) Divide();
-
+        // this code check what button you click out of add, subtract, multipply and divide and then exicute that code
+        // so if you cick equals it will check the if it curently adding or subtracting and complete that cation.
 
     }
 
