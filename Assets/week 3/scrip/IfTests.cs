f965e6232f83838c12c4951d7f854f23d63f0332
Triplethreat36 <149103630+Triplethreat36@users.noreply.github.com>
2024-02-08 19:33:35 -0600

    week 3

---
+++ Assets/week 3/scrip/IfTests.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfTests : MonoBehaviour
+{   
+
+     [ContextMenu("Execute")]
+     void ExecuteTest()
+    {
+        Debug.Log("Test Executed");
+    }
+}
